5ba16da75d3cda59874a2a63768ac66e08a687e0
Alex Hidalgo <aj1hidalgo@gmail.com>
2015-02-16 15:45:46 -0600

    Added files

--- ng-load-button/scripts/controllers.js
+++ ng-load-button/scripts/controllers.js
@@ -1,4 +1,36 @@
-angular.module('app.controllers', [])
-.controller('AppCtrl', function($scope) {
+// angular.module('app.controllers', [])
+// .controller('AppCtrl', function($scope) {
 
+// 	function loadBtn() {
+// 		console.log('button clicked');
+// 	}
+// });
+
+app.controller('LoadButtonCtrl', function($scope, $timeout){
+	// $scope.buttonText = 'Click Me';
+	// $scope.buttonDisabled = false;
+	$scope.buttonText = "Click Me";
+
+	$scope.loadBtn = function() {
+			$scope.buttonText = 'Loading...';
+			$scope.buttonDisabled = true;
+
+			$timeout(function() {
+				// console.log('timeout started');
+				$scope.buttonDisabled = false;
+				$scope.buttonText = "Submitted";
+				// $scope.apply();
+			}, 2000);
+	};
+}).controller('TabCtrl', function($scope){
+
+	$scope.data = {
+		selectedIndex: 0
+	};
+	$scope.next = function() {
+		$scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
+	};
+	$scope.previous = function() {
+    $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
+  };
 });
